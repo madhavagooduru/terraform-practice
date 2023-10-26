784eb5f482f18b82175e48a183d75afa4b06e318
madhavagooduru <madhava.gooduru@gmail.com>
2023-10-26 17:59:44 +0000

    first terraform aws EC2

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "terraform-server" {
+  ami = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-server1"
+  }
+}
+
