3fab2d3f09256da81fe734b472326cecb1d8747b
madhavagooduru <131727011+madhavagooduru@users.noreply.github.com>
2023-10-26 16:07:51 -0400

    Update main.tf
    added security group

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "terraform-server" {
   ami = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.xhtml
@@ -14,4 +15,12 @@ resource "aws_instance" "terraform-server" {
     Name = "terraform-server1"
   }
 }
-
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+}
